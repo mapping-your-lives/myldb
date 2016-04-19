c6884cb9015ee8809fb0d950fcffd6be257f5fb0
tumluliu <nudtlliu@gmail.com>
2016-04-19 21:52:07 +0800

    reformat the code

--- myldb.sql
+++ myldb.sql
@@ -19,84 +19,84 @@ DROP TABLE "transportinfo";
 DROP TABLE "transportmodes";
 
 CREATE TABLE "users" (
-"uid" int8 NOT NULL,
-"gender" varchar(255),
-"email" varchar(255) NOT NULL,
-"birthday" date,
-"deathday" date,
-"education" varchar(255),
-"name" varchar(255),
-"disabled" varchar(255),
-PRIMARY KEY ("uid") 
+    "uid" int8 NOT NULL,
+    "gender" varchar(255),
+    "email" varchar(255) NOT NULL,
+    "birthday" date,
+    "deathday" date,
+    "education" varchar(255),
+    "name" varchar(255),
+    "disabled" varchar(255),
+    PRIMARY KEY ("uid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "events" (
-"eid" int8 NOT NULL,
-"space_id" int8 NOT NULL,
-"started_at" timestamptz(0) NOT NULL,
-"duration" interval(0) NOT NULL,
-"description" varchar(255),
-PRIMARY KEY ("eid") 
+    "eid" int8 NOT NULL,
+    "space_id" int8 NOT NULL,
+    "started_at" timestamptz(0) NOT NULL,
+    "duration" interval(0) NOT NULL,
+    "description" varchar(255),
+    PRIMARY KEY ("eid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "lifelines" (
-"lid" int8 NOT NULL,
-"user_id" int8 NOT NULL,
-PRIMARY KEY ("lid") 
+    "lid" int8 NOT NULL,
+    "user_id" int8 NOT NULL,
+    PRIMARY KEY ("lid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "modes" (
-"mid" int8 NOT NULL,
-"name" varchar(255) NOT NULL,
-"avg_speed" float4 NOT NULL,
-PRIMARY KEY ("mid") 
+    "mid" int8 NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "avg_speed" float4 NOT NULL,
+    PRIMARY KEY ("mid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "participants" (
-"pid" int8 NOT NULL,
-"user_id" int8 NOT NULL,
-"event_id" int8 NOT NULL,
-PRIMARY KEY ("pid") 
+    "pid" int8 NOT NULL,
+    "user_id" int8 NOT NULL,
+    "event_id" int8 NOT NULL,
+    PRIMARY KEY ("pid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "spatialinfo" (
-"sid" int8 NOT NULL,
-"place_id" int8 NOT NULL,
-"description" varchar(255),
-PRIMARY KEY ("sid") 
+    "sid" int8 NOT NULL,
+    "place_id" int8 NOT NULL,
+    "description" varchar(255),
+    PRIMARY KEY ("sid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "mobileprofiles" (
-"mpid" int8 NOT NULL,
-"user_id" int8 NOT NULL,
-"mode_id" int8 NOT NULL,
-"priority" int4 NOT NULL DEFAULT 1,
-PRIMARY KEY ("mpid") 
+    "mpid" int8 NOT NULL,
+    "user_id" int8 NOT NULL,
+    "mode_id" int8 NOT NULL,
+    "priority" int4 NOT NULL DEFAULT 1,
+    PRIMARY KEY ("mpid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "transportinfo" (
-"tid" int8 NOT NULL,
-"cost" float8,
-"duration" interval(0),
-"distance" float8,
-"description" varchar(255),
-"event_id" int8,
-PRIMARY KEY ("tid") 
+    "tid" int8 NOT NULL,
+    "cost" float8,
+    "duration" interval(0),
+    "distance" float8,
+    "description" varchar(255),
+    "event_id" int8,
+    PRIMARY KEY ("tid") 
 )
 WITHOUT OIDS;
 
 CREATE TABLE "transportmodes" (
-"tmid" int8 NOT NULL,
-"mode_id" int8 NOT NULL,
-"transinfo_id" int8 NOT NULL,
-PRIMARY KEY ("tmid") 
+    "tmid" int8 NOT NULL,
+    "mode_id" int8 NOT NULL,
+    "transinfo_id" int8 NOT NULL,
+    PRIMARY KEY ("tmid") 
 )
 WITHOUT OIDS;
 
